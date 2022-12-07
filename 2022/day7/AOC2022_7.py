75896e5ebd2bb4cdb6aa86d0e74c866b6ebe8df1
Jasper Korten <jja2000@gmail.com>
2022-12-07 16:18:35 +0100

    2022: day7: fix last mistakes - Check for /, since that returns to root make a separate if condition - cd'ing into a folder changed currentPath properly but adding to dirList afterwards erroneously appended the new folder again - Python does references to lists instead of copies by default, so to make use of the data in the list instead of the list itself and then appending it (mostly seen with currentPath usage) make a copy beforehand (wildly unoptimized, but who cares) - When assigning sizes to dirs at the end using ":i" will chop off from the front instead of the back, adding a dash to make it negative fixes this so it starts at the back.

--- 2022/day7/AOC2022_7.py
+++ 2022/day7/AOC2022_7.py
@@ -1,4 +1,4 @@
-import sys, time
+import sys
 
 print("Hi! You've used", sys.argv[1], "as input.")
 print("You've chosen to calculate the answer for part", sys.argv[2]+".")
@@ -11,18 +11,19 @@ fileList = []
 
 for l in lines:
     if l[0] == "dir":
-
         dir = currentPath.copy()
         dir.append(l[1])
         if not dirList.count([dir, 0]):
             dirList.append([dir, 0])
+    elif l[1] == "cd" and l[2] == "/":
+        currentPath = [l[2]]
+        if not dirList.count([currentPath, 0]):
+            dirList.append([currentPath.copy(), 0])
     elif l[1] == "cd" and l[2] != "..":
         currentPath.append(l[2])
 
-        dir = currentPath.copy()
-        dir.append(l[2])
-        if not dirList.count([dir, 0]):
-            dirList.append([dir, 0])
+        if not dirList.count([currentPath, 0]):
+            dirList.append([currentPath.copy(), 0])
     elif l[1] == "cd" and l[2] == "..":
         currentPath.pop()
     elif l[0].isdigit():
@@ -34,9 +35,9 @@ for l in lines:
 if sys.argv[2] == '1':
     for f in fileList:
         for i in range(1, len(f[1]) - 1):
-            for j in dirList:
-                if f[1][:i] == j[0]:
-                    j[1] += int(f[0])
+            for d in dirList:
+                if f[1][:-i] == d[0]:
+                    d[1] += int(f[0])
     result = 0
     for d in dirList:
         if d[1] <= 100000:
